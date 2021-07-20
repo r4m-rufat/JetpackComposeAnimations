b2adfb5fda93f37d8cf2f2846847a0e44977d3bb
Rufat Nasirov <rufet.nesirov2001@mail.ru>
2021-07-20 23:40:11 +0400

    circularprogressbar

--- app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -16,14 +18,24 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SimpleColorAnimation()
+//            SimpleColorAnimation()
+            Box(contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()) {
+                CircularProgressBar(percentage = 0.8f, number = 100)
+            }
         }
     }
 }
@@ -42,7 +54,6 @@ fun SimpleColorAnimation(){
     }
 
     val size by animateDpAsState(targetValue = sizeState, tween(durationMillis = 3000, delayMillis = 300, easing = LinearOutSlowInEasing))
-
 
     val infiniteTransition = rememberInfiniteTransition()
 
@@ -66,6 +77,47 @@ fun SimpleColorAnimation(){
         Button(onClick = { sizeState+=50.dp }) {
             Text(text = "Increase Size")
         }
+
+    }
+
+}
+
+@Composable
+fun CircularProgressBar(percentage: Float,
+                number: Int,
+                fontSize: TextUnit = 28.sp,
+                color: Color = Color.Red,
+                strokeWidth: Dp = 5.dp,
+                animationDuration: Int = 1000,
+                animDelay: Int = 200){
+
+    var animationPlay by remember {
+        mutableStateOf(false)
+    }
+
+    val currentPercentage = animateFloatAsState(targetValue = if (animationPlay) percentage else 0f, animationSpec = tween(durationMillis = animationDuration, delayMillis = animDelay))
+
+    LaunchedEffect(key1 = true){
+        animationPlay = true
+    }
+    
+    Box(modifier = Modifier.size(100.dp),
+    contentAlignment = Alignment.Center){
+
+        Canvas(modifier = Modifier.size(100.dp)) {
+            drawArc(
+                color = color,
+                -80f,
+                360 * currentPercentage.value, 
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        
+        Text(text = (currentPercentage.value * number).toInt().toString(),
+        color = Color.Black,
+        fontSize = fontSize,
+        fontWeight =  FontWeight.Bold)
 
     }
 
